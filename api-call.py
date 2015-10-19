c8f8237de3849e138f718d7831004da48cba288e
lauraglasu <lauraglasu@gmail.com>
2015-10-19 11:01:53 +0100

    Refine help and remove some superfluous parentheses and spaces.

--- api-call.py
+++ api-call.py
@@ -6,10 +6,10 @@ import urllib.request
 import requests
 from requests.auth import HTTPBasicAuth
 
-def find_between( s, first, last ):
+def find_between(s, first, last):
     try:
-        start = s.index( first ) + len( first )
-        end = s.index( last, start )
+        start = s.index(first) + len(first)
+        end = s.index(last, start)
         return s[start:end]
     except ValueError:
         return ""
@@ -17,52 +17,46 @@ def find_between( s, first, last ):
 def get_www_authenticate_header(api_url):
     try:
         resp = urllib.request.urlopen(api_url)
-        response = (resp.read())
+        response = resp.read()
     except urllib.error.HTTPError as error:
-        response = (error.info()['Www-Authenticate'])
-    return ( response )
+        response = error.info()['Www-Authenticate']
+    return response 
 
 def get_token(user, password, service, scope, realm):
     data = {"scope":scope, "service":service, "account":user}
     r = requests.post(realm, auth=HTTPBasicAuth(user, password), data=data)
-    token=find_between( (str(r.content)), 'token":"', '"' )
-    return ( token )
+    token=find_between(str(r.content), 'token":"', '"')
+    return token
 
 def get_result(api_url, token):
     r = requests.get(api_url, headers={'Authorization':'Bearer ' + token})
-    return (r.content)
+    return r.content
 
 def main():
 
-    if ( args.user and args.password and args.api_url ):
-        #get the Www-Authenticate header
-        params=(get_www_authenticate_header(args.api_url))
+    #get the Www-Authenticate header
+    params=get_www_authenticate_header(args.api_url)
 
-        #parse the params required for the token
-        if ( params ):
-            realm=find_between( params, 'realm="', '"' )
-            service=find_between( params, 'service="', '"' )
-            scope=find_between( params, 'scope="', '"' )
+    #parse the params required for the token
+    if params:
+        realm=find_between(params, 'realm="', '"')
+        service=find_between(params, 'service="', '"')
+        scope=find_between(params, 'scope="', '"')
 
-            # retrieve token
-            token = get_token(args.user, args.password, service, scope, realm)
+        # retrieve token
+        token = get_token(args.user, args.password, service, scope, realm)
 
-            # Do the API call as an authenticated user
-            print ( "Response:" )
-            print (get_result(args.api_url, token))
-        else:
-            print ( "404 Not Found" )
-
-
+        # Do the API call as an authenticated user
+        print("Response:")
+        print(get_result(args.api_url, token))
     else:
-        # Print usage message in case arguments are missing
-        parser.print_help()
+        print("404 Not Found")
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--user')
-    parser.add_argument('--password')
-    parser.add_argument('--api_url')
+    parser = argparse.ArgumentParser(description='Tool for making api calls to a private docker registry that requires authentication through a cesanta/docker_auth server.')
+    parser.add_argument('--user', required=True, help='Username of the account used to make the request')
+    parser.add_argument('--password', required=True, help='Password of the account used to make the request')
+    parser.add_argument('--api_url', required=True, help='The API url that you want to access.')
     args = parser.parse_args()
 
     main()
